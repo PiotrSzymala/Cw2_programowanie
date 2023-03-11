ff1c8c25e2346968ea5f60e14f292fa8ecd849d8
PiotrSzymala <piotr.szymala@edu.uekat.pl>
2023-03-11 17:39:02 +0100

    Create parameterless and one argument constructor with implemented logic.

--- Cw2_programowanie/Circle.cs
+++ Cw2_programowanie/Circle.cs
@@ -2,5 +2,21 @@ namespace Cw2_programowanie;
 
 public class Circle
 {
-    
+    public double Ray { get; set; }
+    public Circle()
+    {
+        Ray = 1;
+    }
+
+    public Circle(int ray)
+    {
+        if (ray <= 0)
+        {
+            throw new Exception("Ray cannot equal or be less than zero");
+        }
+        else
+        {
+            Ray = ray;
+        }
+    }
 }
