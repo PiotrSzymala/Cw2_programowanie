0def431890d73ca0d1b910e30fdd48befa086d51
PiotrSzymala <piotr.szymala@edu.uekat.pl>
2023-03-16 21:14:39 +0100

    Add logic to name and surname properties

--- Cw2_programowanie/Student.cs
+++ Cw2_programowanie/Student.cs
@@ -2,8 +2,37 @@ namespace Cw2_programowanie;
 
 public class Student
 {
-    public string Name { get; set; }
-    public string Surname { get; set; }
+    private string _name;
+    public string Name
+    {
+        get
+        {
+            return _name;
+        }
+        set
+        {
+            if (value.Length > 2)
+                _name = value;
+        }
+    }
+
+    private string _surname;
+
+    public string Surname
+    {
+        get
+        {
+            return _surname;
+        }
+        set
+        {
+            if (value.Length > 2)
+            {
+                _surname = value;
+            }
+        }
+    }
+    
     public int NrOfTheAlbum { get; set; }
     public double AverageGrade { get; set; }
 }
