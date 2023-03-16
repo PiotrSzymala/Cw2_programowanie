2f0b46b74ab8917a8ebf0ebffa9f7929fa8a5ead
PiotrSzymala <piotr.szymala@edu.uekat.pl>
2023-03-16 21:06:58 +0100

    Create Student class.

---
+++ Cw2_programowanie/Student.cs
@@ -0,0 +1,9 @@
+namespace Cw2_programowanie;
+
+public class Student
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public int NrOfTheAlbum { get; set; }
+    public double AverageGrade { get; set; }
+}
