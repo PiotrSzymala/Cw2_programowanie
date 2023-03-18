0adaa9eed0f24619a9500fd37fe3c38a324972c4
PiotrSzymala <piotr.szymala@edu.uekat.pl>
2023-03-18 15:58:14 +0100

    Create client class.

---
+++ Cw2_programowanie/Client.cs
@@ -0,0 +1,14 @@
+﻿namespace Cw2_programowanie;
+
+public class Client
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+
+    public Client(string name, string surname)
+    {
+        Name = name;
+        Surname = surname;
+    }
+
+}
