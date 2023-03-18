9d16ebdb696b5c80065f0e6ff802ebfceebd3e43
PiotrSzymala <piotr.szymala@edu.uekat.pl>
2023-03-18 16:04:20 +0100

    Create class to display ordered products.

---
+++ Cw2_programowanie/OrderDisplayer.cs
@@ -0,0 +1,14 @@
+﻿namespace Cw2_programowanie;
+
+public static class OrderDisplayer
+{
+    public static void Display(Order order)
+    {
+        Console.WriteLine($"{order.PersonWhoOrdered}'s order: ");
+
+        foreach (KeyValuePair<string,int> product in order.OrderedProducts)
+        {
+            Console.WriteLine($"Product: {product.Key}, quantity: {product.Value}");
+        }
+    }
+}
