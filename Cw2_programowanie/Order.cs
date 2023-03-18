149639846470b485686fd73cc7c65d35ae73414e
PiotrSzymala <piotr.szymala@edu.uekat.pl>
2023-03-18 15:58:24 +0100

    Create order class.

---
+++ Cw2_programowanie/Order.cs
@@ -0,0 +1,14 @@
+﻿namespace Cw2_programowanie;
+
+public class Order
+{
+    public Client PersonWhoOrdered { get; set; }
+    public List<Dictionary<string, int>> OrderedProducts { get; set; }
+
+
+    public Order(Client personWhoOrdered, List<Dictionary<string,int>> orderedProducts)
+    {
+        PersonWhoOrdered = personWhoOrdered;
+        OrderedProducts = orderedProducts;
+    }
+}
