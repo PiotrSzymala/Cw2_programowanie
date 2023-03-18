3ac7d71f34b04a09897e8ec7e313b3105d8b08da
PiotrSzymala <piotr.szymala@edu.uekat.pl>
2023-03-18 16:06:02 +0100

    OrderedProducts property change.

--- Cw2_programowanie/Order.cs
+++ Cw2_programowanie/Order.cs
@@ -3,10 +3,10 @@
 public class Order
 {
     public Client PersonWhoOrdered { get; set; }
-    public List<Dictionary<string, int>> OrderedProducts { get; set; }
+    public Dictionary<string, int> OrderedProducts { get; set; }
 
 
-    public Order(Client personWhoOrdered, List<Dictionary<string,int>> orderedProducts)
+    public Order(Client personWhoOrdered, Dictionary<string,int> orderedProducts)
     {
         PersonWhoOrdered = personWhoOrdered;
         OrderedProducts = orderedProducts;
