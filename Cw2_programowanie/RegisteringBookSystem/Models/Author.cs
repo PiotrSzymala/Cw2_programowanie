d7818f0006b0ee5c843ed25e42d293f61550e881
PiotrSzymala <piotr.szymala@edu.uekat.pl>
2023-03-20 20:03:08 +0100

    Make Property Pseudonym nullable.

--- Cw2_programowanie/RegisteringBookSystem/Models/Author.cs
+++ Cw2_programowanie/RegisteringBookSystem/Models/Author.cs
@@ -5,9 +5,9 @@ public class Author
     public string Name { get; set; }
     public string? SecondName { get; set; }
     public string Surname { get; set; }
-    public string Pseudonym{ get; set; }
+    public string? Pseudonym{ get; set; }
 
-    public Author(string name, string? secondName, string surname, string pseudonym)
+    public Author(string name, string? secondName, string surname, string? pseudonym)
     {
         Name = name;
         SecondName = secondName;
