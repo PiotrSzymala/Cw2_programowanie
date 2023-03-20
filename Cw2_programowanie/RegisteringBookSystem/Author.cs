a8ab75517f09b05f787a0d75eb62c05d0d92347a
PiotrSzymala <piotr.szymala@edu.uekat.pl>
2023-03-20 18:29:55 +0100

    Create Author class.

---
+++ Cw2_programowanie/RegisteringBookSystem/Author.cs
@@ -0,0 +1,17 @@
+namespace Cw2_programowanie.RegisteringBookSystem;
+
+public class Author
+{
+    public string Name { get; set; }
+    public string SecondName { get; set; }
+    public string Surname { get; set; }
+    public string Pseudonym{ get; set; }
+
+    public Author(string name, string secondName, string surname, string pseudonym)
+    {
+        Name = name;
+        SecondName = secondName;
+        Surname = surname;
+        Pseudonym = pseudonym;
+    }
+}
