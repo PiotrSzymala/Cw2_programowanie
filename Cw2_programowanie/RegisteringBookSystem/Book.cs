88a4c84a7e1c046efd6a1560254a215726360edc
PiotrSzymala <piotr.szymala@edu.uekat.pl>
2023-03-20 18:36:06 +0100

    Create Book class.

---
+++ Cw2_programowanie/RegisteringBookSystem/Book.cs
@@ -0,0 +1,21 @@
+namespace Cw2_programowanie.RegisteringBookSystem;
+
+public class Book
+{
+    public List<Author> Authors { get; set; }
+    public string Title { get; set; }
+    public string OriginalTitle { get; set; }
+    public DateTime PublicationDate { get; set; }
+    public string PublishingCompany { get; set; }
+    public int ISBN { get; set; }
+
+    public Book(List<Author> authors, string title, string originalTitle, DateTime publicationDate, string publishingCompany, int isbn)
+    {
+        Authors = authors;
+        Title = title;
+        OriginalTitle = originalTitle;
+        PublicationDate = publicationDate;
+        PublishingCompany = publishingCompany;
+        ISBN = isbn;
+    }
+}
