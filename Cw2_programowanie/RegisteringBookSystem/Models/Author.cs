a5f208bcd322a486f505bad3cffec06c4b48c922
PiotrSzymala <piotr.szymala@edu.uekat.pl>
2023-03-20 18:55:17 +0100

    Small changes in Author class.

--- Cw2_programowanie/RegisteringBookSystem/Models/Author.cs
+++ Cw2_programowanie/RegisteringBookSystem/Models/Author.cs
@@ -3,11 +3,11 @@ namespace Cw2_programowanie.RegisteringBookSystem.Models;
 public class Author
 {
     public string Name { get; set; }
-    public string SecondName { get; set; }
+    public string? SecondName { get; set; }
     public string Surname { get; set; }
     public string Pseudonym{ get; set; }
 
-    public Author(string name, string secondName, string surname, string pseudonym)
+    public Author(string name, string? secondName, string surname, string pseudonym)
     {
         Name = name;
         SecondName = secondName;
