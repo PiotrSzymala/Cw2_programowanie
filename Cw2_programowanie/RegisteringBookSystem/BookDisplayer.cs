2f38fd3921721265a6ffc6e920b008dec36b1703
PiotrSzymala <piotr.szymala@edu.uekat.pl>
2023-03-21 22:01:18 +0100

    Change format of displaying.

--- Cw2_programowanie/RegisteringBookSystem/BookDisplayer.cs
+++ Cw2_programowanie/RegisteringBookSystem/BookDisplayer.cs
@@ -12,13 +12,17 @@ public static class BookDisplayer
             Console.WriteLine("Author: ");
             foreach (var author in book.Authors)
             {
-                Console.WriteLine(author);
+                Console.WriteLine("\t"+author);
             }
 
             Console.WriteLine($"Title: {book.Title}");
             Console.WriteLine($"Original title: {book.OriginalTitle}");
             Console.WriteLine($"Publication date: {book.PublicationDate}");
-            Console.WriteLine($"Publishing company: {book.PublishingCompany}");
+            Console.WriteLine($"Publishing company: ");
+            foreach (var publisher in book.PublishingCompany)
+            {
+                Console.WriteLine("\t"+publisher);
+            }
             Console.WriteLine($"ISBN number: {book.ISBN}");
             Console.WriteLine();
             Console.WriteLine();
