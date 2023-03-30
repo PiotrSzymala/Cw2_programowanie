2a3b4941c6f46157727c14fc7463bc750328e4c7
PiotrSzymala <piotr.szymala@edu.uekat.pl>
2023-03-30 21:15:19 +0200

    Create method that returns smallest non negative integer whose individual digits sum to given integer.

--- Cw2_programowanie/Program.cs
+++ Cw2_programowanie/Program.cs
@@ -91,3 +91,38 @@ BookManager.Run();
 
      return text;
  }
+ 
+static int SumIndividualDigits(int N)
+{
+    List<int> numbersThatMeetCondition = new List<int>();
+
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            for (int k = 0; k < 10; k++)
+            {
+                for (int l = 0; l < 10; l++)
+                {
+                    for (int m = 0; m < 10; m++)
+                    {
+                        for (int n = 0; n < 10; n++)
+                        {
+                            if (i + j + k + l + m + n == N)
+                            {
+                                string numbersToString = (i + j.ToString() + k.ToString()+l.ToString() +m.ToString() +n.ToString());
+
+                                if (int.TryParse(numbersToString, out int b))
+                                {
+                                    numbersThatMeetCondition.Add(b);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return numbersThatMeetCondition.Min();
+}
