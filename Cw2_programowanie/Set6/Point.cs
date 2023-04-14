eb50e1ff456ee73e7cf9732decaaa5dd0dce13b1
piotrszymala <piotr.szymala@edu.uekat.pl>
2023-04-14 20:27:31 +0200

    Create point class with properties and overrided operators.

---
+++ Cw2_programowanie/Set6/Point.cs
@@ -0,0 +1,35 @@
+namespace Cw2_programowanie.Set6;
+
+public class Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public static Point operator *(Point point, int k)
+    {
+        return new Point(point.X * k, point.Y * k);
+    }
+    
+    public static bool operator ==(Point p1, Point p2)
+    {
+        if (p1.X == p2.X && p1.Y == p2.Y)
+            return true;
+
+        return false;
+    }
+
+    public static bool operator !=(Point p1, Point p2)
+    {
+        if (p1.X != p2.X && p1.Y != p2.Y)
+            return true;
+
+        return false;
+    }
+    
+}
