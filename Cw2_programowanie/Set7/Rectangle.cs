2fab273a6b0e7adff469ae5dcf7322e710c2da0c
piotrszymala <piotr.szymala@edu.uekat.pl>
2023-04-15 14:35:36 +0200

    Create Rectangle class as additional class to use in Cylinder.cs

---
+++ Cw2_programowanie/Set7/Rectangle.cs
@@ -0,0 +1,13 @@
+namespace Cw2_programowanie.Set7;
+
+public class Rectangle
+{
+    public double A { get; set; }
+    public double B { get; set; }
+
+    public Rectangle(double a, double b)
+    {
+        A = a;
+        B = b;
+    }
+}
